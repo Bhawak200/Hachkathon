dfd34dc5e23490da8fc38dbc1856cfcc1909bd0a
satakshi <satakshi.pal.civ19@itbhu.ac.in>
2021-05-08 20:26:19 +0530

    added header and footer files

--- health-app/src/App.js
+++ health-app/src/App.js
@@ -1,12 +1,24 @@
 
 import Carouselpart from './Components/Carouselpart'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/header'
+import Footer from './Components/footer'
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link
+ } from "react-router-dom";
 
 function App() {
   return (
+     <Router>
      <div>
+        <Header> </Header>
         <Carouselpart></Carouselpart>
+        <Footer></Footer>
      </div>
+     </Router>
   );
 }
 
