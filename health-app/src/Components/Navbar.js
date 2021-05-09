18f0b9e3525e4975c9b1ecf22aae356807bc970c
satakshi <satakshi.pal.civ19@itbhu.ac.in>
2021-05-09 11:00:11 +0530

    changed navbar and footer

---
+++ health-app/src/Components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Register</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+
