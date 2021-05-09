3d2549b4c2109f5f8a61cd73419a6a2514ed53d4
Bhawak200 <bhawakanand1996@gmail.com>
2021-05-09 12:20:20 +0530

    changes

--- health-app/src/App.js
+++ health-app/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import 	Navbar from "./Components/Navbar"
+import Footer from "./Components/footer"
 import Login from "./Components/Login";
 import Main from "./Components/Main";
 import Doctors from "./Components/Doctors";
@@ -7,11 +9,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
 	return (
 		<Router>
+		<Navbar> </Navbar>
 			<Switch>
 				<Route exact path="/" component={Main}></Route>
 				<Route exact path="/login" component={Login}></Route>
 				<Route exact path="/doctors" component={Doctors}></Route>
 			</Switch>
+			<Footer></Footer>
 		</Router>
 	);
 };
--- health-app/src/Components/Navbar.js
+++ health-app/src/Components/Navbar.js
@@ -13,21 +13,9 @@ const Navbar = () => {
     <>
       <Nav>
         <Bars />
-        <div className="h1 company-name" >
+        <NavLink to = "/" className="h1 company-name" >
             Medic
-          </div>
-        <NavMenu>
-        
-          <NavLink to='/' activeStyle  className="h3">
-            Home
           </NavLink>
-          <NavLink to='/about' activeStyle className="h3">
-            About
-          </NavLink>
-          <NavLink to='/contact-us' activeStyle className="h3">
-            Contact Us
-          </NavLink>
-        </NavMenu>
         <NavBtn>
           <NavBtnLink to='/login'className="h3">Register</NavBtnLink>
         </NavBtn>
